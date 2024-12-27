8885374945e3edada48ec2acd7412679e3d133b5
Danill163 <rdn-06@mail.ru>
2024-12-27 20:13:10 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,11 @@
+user_name = "Артем"
+title = "Подтягивания"
+content = "Увеличение кол-ва подтягиваний на перекладине"
+status = "Активно"
+created_date = "01-01-2025"
+issue_date = "01-03-2025"
+print("Имя пользователя:", user_name)
+print("Заметка:", title)
+print("Описание:", content)
+print("Дата создания:",created_date,)
+print("Дата истечения заметки:", issue_date)
