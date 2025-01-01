02847851cd4f906f84f34090286d7db298cce888
DanilRublev <rdn-06@mail.ru>
2025-01-01 18:17:36 +0300

    date_changer.py

---
+++ date_changer.py
@@ -0,0 +1,13 @@
+user_name = "Артем"
+title = "Подтягивания"
+content = "Увеличение кол-ва подтягиваний на перекладине"
+status = "Активно"
+created_date = "01-Jan-2025"
+issue_date = "01-Mar-2025"
+temp_created_date = (created_date[0:6])
+temp_issue_date = (issue_date[0:6])
+print("Имя пользователя:", user_name)
+print("Заметка:", title)
+print("Описание:", content)
+print("Дата создания:",temp_created_date,)
+print("Дата истечения заметки:", temp_issue_date)
